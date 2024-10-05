9e164c7aa5d980b93b84d4d11bb62f09a0bbc611
Ash Kestrel <tetrapteryxgames@gmail.com>
2024-10-05 16:44:34 -0300

    Give humans parasites

--- scripts/Facility.gd
+++ scripts/Facility.gd
@@ -3,12 +3,15 @@ extends Node2D
 export(Array, PackedScene) var levels = []
 export var debug_level : int = 0
 export var room_center_pos : Vector2 = Vector2.ZERO
+export var contamination_chance = 0.6
+export var max_contamination_chance = 0.9
+export var growth_rate = 0.333
 
 var contamination_percent : float = 0.0
 var population : int = 0
 
 var current_level : Node2D = null
-var current_level_scene : PackedScene = null
+var level_index = 0
 var human_queue : Array = []
 var human_index = 0
 
@@ -19,30 +22,10 @@ onready var humans = $Humans
 
 
 func _ready():
-	update_population()
-	if OS.is_debug_build() and debug_level:
-		load_level(levels[debug_level])
-	else:
-		next_level()
-
-func next_human():
-	human_index += 1
-	if human_index >= human_queue.size():
-		next_level()
-		return
+	init_state()
 	enter_human()
 
-func exit_human():
-	disable_actions()
-	var human : Human = human_queue[human_index]
-	human.connect("exited", self, "next_human")
-	human.exit()
-
-func enter_human():
-	var human : Human = human_queue[human_index]
-	human.connect("entered", self, "enable_actions")
-	human.enter()
-
+#### PLAYER ACTIONS ####
 func accept_human():
 	print("accepted")
 	exit_human()
@@ -58,8 +41,21 @@ func disable_actions():
 func enable_actions():
 	for action in actions.get_children():
 		(action as Button).disabled = false
+########################
 
-func contamination_percent():
+#### SETUP ####
+func init_state():
+	update_contamination()
+	update_population()
+	for human in humans.get_children():
+		human.connect("exited", self, "next_human")
+		human.connect("entered", self, "enable_actions")
+	
+	if OS.is_debug_build():
+		level_index = debug_level
+	load_level(levels[level_index])
+
+func update_contamination():
 	var display_format = "Contamination: %.1f%%"
 	contamination_level.text = display_format % contamination_percent
 
@@ -68,21 +64,10 @@ func update_population():
 	var display_format = "Population: %d"
 	population_count.text = display_format % population
 
-func next_level():
-	if levels.empty():
-		return
-
-	var level_scene = levels.pop_front()
-	load_level(level_scene)
-
-func restart_level():
-	load_level(current_level_scene)
-
 func load_level(level):
-	current_level_scene = level
 	if current_level:
 		current_level.queue_free()
-	current_level = current_level_scene.instance()
+	current_level = level.instance()
 	add_child(current_level)
 	reset_humans()
 
@@ -91,4 +76,60 @@ func reset_humans():
 	for human in human_queue:
 		human.reset()
 	human_index = 0
+###############
+
+func next_human():
+	recalculate_contamination()
+	human_index += 1
+	if human_index >= human_queue.size():
+		end_level()
+		return
+	enter_human()
+
+func enter_human():
+	var human : Human = human_queue[human_index]
+	contaminate_human(human)
+	human.enter()
+
+func exit_human():
+	disable_actions()
+	var human : Human = human_queue[human_index]
+	human.exit()
+
+func contaminate_human(human):
+	# TODO make this make sense
+	human.contaminate(clamp(contamination_percent + contamination_chance, 0, max_contamination_chance), growth_rate)
+
+func recalculate_contamination():
+	var total_contam = 0
+	var valid_humans = 0
+	for human in human_queue:
+		if is_instance_valid(human):
+			valid_humans += 1
+			total_contam += human.contamination_percent
+	print(total_contam)
+	print(valid_humans)
+	print(total_contam / valid_humans)
+	contamination_percent = total_contam / valid_humans * 100
+	update_contamination()
+
+func end_level():
+	if contamination_percent:
+		# Spread
+		for human in humans.get_children():
+			contaminate_human(human)
+		recalculate_contamination()
+			
+	print("Level complete")
+	# TODO level report
+	next_level()
+
+func next_level():
+	level_index += 1
+		
+	if level_index >= levels.size():
+		print("Game over")
+		return
+
+	load_level(levels[level_index])
 	enter_human()
--- scripts/Human.gd
+++ scripts/Human.gd
@@ -9,12 +9,36 @@ export var spawn_pos : Vector2 = Vector2(376, 0)
 export var center_pos : Vector2 = Vector2.ZERO
 export var exit_pos : Vector2 = Vector2(-376, 0)
 export var move_duration : float = 1
+export var max_particles : int = 10
+
+var contamination_percent : float = 0.0
 
 onready var tween : Tween = $Tween
+onready var contamination : CPUParticles2D = $Contamination
 
+
+func _ready():
+	randomize()
 
 func reset():
 	position = spawn_pos
+
+func decontaminate():
+	contamination_percent = 0
+	contamination.emitting = false
+
+func contaminate(probability, growth_rate):
+	if contamination_percent:
+		contamination_percent = clamp(contamination_percent + growth_rate, 0, 1)
+	elif rand_range(0.0, 1.0) < probability:
+		contamination_percent = growth_rate
+		contamination.emitting = true
+
+	if not contamination_percent:
+		return
+
+	var particle_count = round(max_particles * contamination_percent)
+	contamination.amount = clamp(particle_count, 1, max_particles)
 
 func enter():
 	move_pos(spawn_pos, center_pos, "entered")
